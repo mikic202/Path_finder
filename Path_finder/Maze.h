1346a9b9105be62956ba4f67f7f8faf7b235834f
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-07-27 21:27:22 +0200

    declared maze class

--- Path_finder/Maze.h
+++ Path_finder/Maze.h
@@ -1,1 +1,19 @@
-#pragma once
+#ifndef Maze_H
+#define Maze_H
+#include "GridSpace.h"
+#include <string>
+
+
+class Maze
+{
+private:
+	std::vector<GridSpace> Grid;
+public:
+	Maze();
+	void generte_maze(std::vector<int> size);
+	void maze_from_file(std::string file_name);
+};
+
+
+#endif // !Maze_H
+
