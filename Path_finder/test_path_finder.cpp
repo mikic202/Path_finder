0cc80371a0ffa2e7bd57c597905237c791c3354a
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-07-28 21:19:42 +0200

    added const file

--- Path_finder/test_path_finder.cpp
+++ Path_finder/test_path_finder.cpp
@@ -1,2 +1,16 @@
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include "const.h"
 #include "GridSpace.h"
+
+
+TEST_CASE("test GridSpace class")
+{
+	SECTION("test initialization")
+	{
+		GridSpace space(EMPTY_SPACE, { 1, 2 });
+
+		REQUIRE(space.space_state() == EMPTY_SPACE);
+		REQUIRE(space.grid_position()[0] == 1);
+	}
+};
