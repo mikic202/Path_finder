de74eb7699db60b0f426fa6508a2c20d76a0db08
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-08-02 20:44:16 +0200

    declared window class

--- Path_finder/Window.h
+++ Path_finder/Window.h
@@ -1,10 +1,17 @@
 #ifndef Window_H
 #define Window_H
+#include "PathFinder.h"
 
 
 class Window
 {
-
+private:
+	Maze maze_;
+	PathFinder path_finder_;
+	sf::RenderWindow window_;
+public:
+	Window();
+	void open_window();
 };
 
 
