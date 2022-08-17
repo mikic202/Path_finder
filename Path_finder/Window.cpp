622bb50b7edf63b33ee688318cb21f57d11c961f
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-08-17 13:48:20 +0200

    added feature that fills most of the screen with maze no mater the size of the maze

--- Path_finder/Window.cpp
+++ Path_finder/Window.cpp
@@ -4,37 +4,25 @@
 Window::Window()
 {
 	maze_ = Maze();
-	maze_.generte_maze({ 30, 30 }, {27, 15}, {2, 1});
+	maze_.generte_maze({ 10, 10 }, {7, 7}, {2, 1});
 	path_finder_ = PathFinder();
 }
 
 void Window::open_window()
 {
-    sf::RenderWindow window_(sf::VideoMode(1000, 1000), "Pathfinder");
+    sf::RenderWindow window_(sf::VideoMode(800, 800), "Pathfinder");
     path_finder_.set_maze(maze_);
     int i = 0;
-    sf::RectangleShape rects[1825];
-    float tile_size = 15;
-    for (auto space : maze_.grid())
-    {
-        if (space.space_state() == FULL_SPACE)
-        {
-            rects[i].setSize({ tile_size, tile_size });
-            rects[i].setPosition({ float(space.grid_position()[0] * (tile_size + 4) + 1), float(space.grid_position()[1] * (tile_size + 4) + 1) });
-            rects[i].setFillColor(sf::Color::Green);
-            window_.draw(rects[i]);
-            i++;
-        }
-    }
+    generate_maze_grid(window_);
     std::vector<GridSpace> path_ = path_finder_.solve_maze_sample();
-    for (auto space : path_)
-    {
-        rects[i].setSize({ tile_size, tile_size });
-        rects[i].setPosition({ float(space.grid_position()[0] * (tile_size + 4) + 1), float(space.grid_position()[1] * (tile_size + 4) + 1) });
-        rects[i].setFillColor(sf::Color::Blue);
-        window_.draw(rects[i]);
-        i++;
-    }
+    //for (auto space : path_)
+    //{
+    //    rects[i].setSize({ tile_size, tile_size });
+    //    rects[i].setPosition({ float(space.grid_position()[0] * (tile_size + 4) + 1), float(space.grid_position()[1] * (tile_size + 4) + 1) });
+    //    rects[i].setFillColor(sf::Color::Blue);
+    //    window_.draw(rects[i]);
+    //    i++;
+    //}
     window_.display();
 	while (window_.isOpen())
 	{
@@ -47,21 +35,30 @@ void Window::open_window()
                 exit(1);
             }
         }
-        //draw_maze_(window_);
+        draw_maze_(window_);
+        window_.display();
 	}
 }
 
 void Window::draw_maze_(sf::RenderWindow& window)
 {
+    for (auto space : rects_)
+    {
+        window.draw(space);
+    }
+}
+
+void Window::generate_maze_grid(sf::RenderWindow& window)
+{
+    float tile_size = std::min({ window.getSize().x/maze_.maze_size()[0] - 4, window.getSize().y/maze_.maze_size()[1] - 4});
     for (auto space : maze_.grid())
     {
         if (space.space_state() == FULL_SPACE)
         {
-            sf::RectangleShape rect;
-            rect.setSize({ 1, 1 });
-            rect.setPosition({ float(space.grid_position()[0] * 30), float(space.grid_position()[1] * 30) });
-            rect.setFillColor(sf::Color::Black);
-            window.draw(rect);
+            rects_.push_back(sf::RectangleShape());
+            rects_.back().setSize({ tile_size, tile_size });
+            rects_.back().setPosition({ float(space.grid_position()[0] * (tile_size + 4) + 1), float(space.grid_position()[1] * (tile_size + 4) + 1) });
+            rects_.back().setFillColor(sf::Color::Green);
         }
     }
 }
--- Path_finder/Window.h
+++ Path_finder/Window.h
@@ -7,9 +7,11 @@
 class Window
 {
 private:
+	std::vector<sf::RectangleShape> rects_;
 	Maze maze_;
 	PathFinder path_finder_;
 	void draw_maze_(sf::RenderWindow& window);
+	void generate_maze_grid(sf::RenderWindow& window);
 public:
 	Window();
 	void open_window();
