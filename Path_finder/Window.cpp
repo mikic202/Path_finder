cd8f0a600835ab6b16825122e62b0aa80851f4f4
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-08-18 14:46:52 +0200

    added simple start menu

--- Path_finder/Window.cpp
+++ Path_finder/Window.cpp
@@ -4,8 +4,12 @@
 
 Window::Window()
 {
+    if (!font_.loadFromFile(FONT_PATH))
+    {
+        throw(std::exception());
+    }
 	maze_ = Maze();
-	maze_.generte_maze({ 10, 10 });
+	maze_.generte_maze({ 30, 30 });
 	path_finder_ = PathFinder();
 }
 
@@ -14,8 +18,15 @@ void Window::open_window()
     sf::RenderWindow window_(sf::VideoMode(800, 800), "Pathfinder");
     path_finder_.set_maze(maze_);
     float tile_size = generate_maze_grid(window_);
-    std::vector<GridSpace> path_ = path_finder_.solve_maze_sample();
+    std::vector<int> options = start_menu(window_);
+    std::vector<GridSpace> path_;
+    if(options[2] == 2)
+        path_ = path_finder_.solve_maze_sample();
+    else if (options[2] == 1)
+        path_ = path_finder_.solve_maze();
     int i = 0;
+    buttons_.clear();
+    window_.clear();
     window_.display();
 	while (window_.isOpen())
 	{
@@ -29,8 +40,7 @@ void Window::open_window()
             }
         }
         draw_maze_(window_);
-        window_.display();
-        if (i < path_.size()-1)
+        if (i < path_.size())
         {
             update_path_(path_, i, tile_size);
             i++;
@@ -40,10 +50,12 @@ void Window::open_window()
 
 void Window::draw_maze_(sf::RenderWindow& window)
 {
+    window.clear();
     for (auto space : rects_)
     {
         window.draw(space);
     }
+    window.display();
 }
 
 float Window::generate_maze_grid(sf::RenderWindow& window)
@@ -68,5 +80,39 @@ void Window::update_path_(std::vector<GridSpace> path, int index_num, float tile
     rects_.back().setSize({ tile_size, tile_size });
     rects_.back().setPosition({ float(path[index_num].grid_position()[0] * (tile_size + 4) + 1), float(path[index_num].grid_position()[1] * (tile_size + 4) + 1)});
     rects_.back().setFillColor(sf::Color::Blue);
-    Sleep(300);
+    Sleep(200);
+}
+
+std::vector<int> Window::start_menu(sf::RenderWindow& window)
+{
+    int winwow_w = window.getSize().x;
+
+    buttons_.insert(std::pair<int, Button>(1, Button({ float(winwow_w) / 2, 300.f }, 50, sf::Color::Yellow, "Recursive", font_)));
+    buttons_.insert(std::pair<int, Button>(2, Button({ float(winwow_w) / 2, 450.f }, 50, sf::Color::Yellow, "Sample", font_)));
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+                exit(1);
+            }
+        }
+        for (auto button_pair : buttons_)
+        {
+            if (button_pair.second.is_pressed(window))
+            {
+                return { 0, 0, button_pair.first };
+            }
+        }
+        window.clear();
+        for (auto buttons : buttons_)
+        {
+            buttons.second.draw_to(window);
+        }
+        window.display();
+    }
+    return { 0, 0, 0 };
 }
--- Path_finder/Window.h
+++ Path_finder/Window.h
@@ -2,17 +2,21 @@
 #define Window_H
 #include "PathFinder.h"
 #include <SFML/Graphics.hpp>
+#include "Button.h"
 
 
 class Window
 {
 private:
+	sf::Font font_;
 	std::vector<sf::RectangleShape> rects_;
+	std::map<int, Button> buttons_;
 	Maze maze_;
 	PathFinder path_finder_;
 	void draw_maze_(sf::RenderWindow& window);
 	float generate_maze_grid(sf::RenderWindow& window);
 	void update_path_(std::vector<GridSpace> path, int index_num, float tile_size);
+	std::vector<int> start_menu(sf::RenderWindow& window);
 public:
 	Window();
 	void open_window();
