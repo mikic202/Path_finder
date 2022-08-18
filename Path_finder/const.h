c36af2179551fb444118db656fc7955163b03319
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-08-18 14:44:39 +0200

    added font

--- Path_finder/const.h
+++ Path_finder/const.h
@@ -1,7 +1,10 @@
 #ifndef const_H
 #define const_H
+#include <string>
 
 int const EMPTY_SPACE = -1;
 int const FULL_SPACE = -2;
+
+const std::string FONT_PATH = "Nasa21-l23X.ttf";
 
 #endif // !const_H
